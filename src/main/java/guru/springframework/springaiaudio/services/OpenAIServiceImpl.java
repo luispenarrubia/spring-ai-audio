5326dce658c15555348915988e227341c334d117
Luis Peñarrubia <luis.penarrubia@gmail.com>
2025-03-05 12:41:11 +0100

    spring ai - text to speech

--- src/main/java/guru/springframework/springaiaudio/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiaudio/services/OpenAIServiceImpl.java
@@ -1,6 +1,12 @@
 package guru.springframework.springaiaudio.services;
 
+import guru.springframework.springaiaudio.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.ai.openai.audio.speech.SpeechPrompt;
+import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,5 +16,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
+    private final OpenAiAudioSpeechModel speechModel;
 
+    @Override
+    public byte[] getSpeech(Question question) {
+        OpenAiAudioSpeechOptions options = OpenAiAudioSpeechOptions.builder()
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ONYX)
+                .speed(1.0f)
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .model(OpenAiAudioApi.TtsModel.TTS_1_HD.value)
+                .build();
+
+        SpeechPrompt prompt = new SpeechPrompt(question.question(), options);
+
+        SpeechResponse response = speechModel.call(prompt);
+
+        return response.getResult().getOutput();
+    }
 }
